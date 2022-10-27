fbcdb4ae696a8af7c1732beadfaa7f53cf7aa6cb
Marcus Wu <marcus.wu0814@gmail.com>
2022-10-27 15:20:46 +0800

    Add Example project

---
+++ Example/Example/ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleApp.swift
+//  Example
+//
+//  Created by Marcus Wu on 2022/10/27.
+//
+
+import SwiftUI
+
+@main
+struct ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
